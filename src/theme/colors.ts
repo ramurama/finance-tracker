ce5a856fc62b1d8b4a4f8bab1c837304e207d479
Ramu Ramasamy <rram.ramasamy@gmail.com>
2022-11-06 08:35:19 +0100

    dark color changed

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -3,13 +3,13 @@ import { ColorType } from './ThemeContext'
 export const lightColors: ColorType = {
   background: '#f2f2f2',
   secondaryBackground: '#f2f2f2',
-  text: '#212121',
+  text: '#1b1b1b',
   tabIconActiveColor: '#212121',
   tabIconInactiveColor: '#bdbdbd',
 } as const
 
 export const darkColors: ColorType = {
-  background: '#212121',
+  background: '#1b1b1b',
   secondaryBackground: '#212121',
   text: '#ffffff',
   tabIconActiveColor: '#f2f2f2',
