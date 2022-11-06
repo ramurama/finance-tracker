d2ebeb0ccdbfd67b97a95a8a93b7db4b285d3434
Ramu Ramasamy <rram.ramasamy@gmail.com>
2022-11-06 12:51:53 +0100

    Header component,

--- src/components/fragments/Header.tsx
+++ src/components/fragments/Header.tsx
@@ -1,22 +1,66 @@
-import { FC } from 'react'
-import { StyleSheet, View } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/core'
+import { FC, ReactNode } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
-import { CustomButton } from '../atoms'
+import { useTheme } from '../../theme'
 
 interface HeaderProps {
-  title: string
+  title?: string
+  iconRight?: ReactNode
+  backButton?: boolean
 }
 
-export const Header: FC<HeaderProps> = ({ title }) => {
+export const Header: FC<HeaderProps> = ({ title, iconRight, backButton }) => {
+  const { colors } = useTheme()
+  const navigation = useNavigation()
+
   return (
-    <View style={styles.headerContainer}>
-      <CustomButton label={title} onPress={() => {}} />
+    <View style={{ ...styles.headerContainer, backgroundColor: colors.background }}>
+      {backButton && (
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => {
+            navigation.goBack()
+          }}>
+          <MaterialIcons name="arrow-back-ios" color={colors.text} size={24} />
+        </TouchableOpacity>
+      )}
+      <View style={styles.titleView}>
+        {title && <Text style={{ ...styles.title, color: colors.text }}>{title}</Text>}
+      </View>
+      {iconRight && <View style={styles.iconContainer}>{iconRight}</View>}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   headerContainer: {
-    display: 'flex',
+    height: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  titleView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '700',
+  },
+  iconContainer: {
+    position: 'absolute',
+    top: 0,
+    right: 10,
+  },
+  backButton: {
+    position: 'absolute',
+    top: 0,
+    left: 15,
+    height: 30,
+    width: 30,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
 })
