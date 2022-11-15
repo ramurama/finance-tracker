f5ce018fa8ff10cbd08bb02e80eb8da551aee4d2
Ramu Ramasamy <rram.ramasamy@gmail.com>
2022-11-15 10:57:58 +0530

    formik and yup implementation for books

--- src/screens/Books/CreateBook.tsx
+++ src/screens/Books/CreateBook.tsx
@@ -1,28 +1,40 @@
-import { useState } from 'react'
+import { Formik } from 'formik'
+import * as yup from 'yup'
 
 import { Container } from '../../components'
-import { InputField } from '../../components/atoms'
+import { Button, InputField } from '../../components/atoms'
 import { Header } from '../../components/fragments'
 import { i18n } from '../../locales'
 
 export const CreateBook = () => {
-  const [bookName, setBookName] = useState<string>('Germany')
+  const bookValidationSchema = yup.object().shape({
+    bookName: yup.string().required(i18n.t('books.errorBookName')),
+  })
 
   return (
     <>
       <Container>
         <Header title={i18n.t('books.createBook')} backButton />
-        <InputField
-          label="Name"
-          error="Enter a valid book name."
-          isErrorVisible={true}
-          placeholder="Book Name"
-          value={bookName}
-          onChange={(value) => {
-            setBookName(value)
-          }}
-          keyboardType="default"
-        />
+
+        <Formik
+          validationSchema={bookValidationSchema}
+          initialValues={{ bookName: '' }}
+          onSubmit={(values) => console.log(values)}>
+          {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
+            <>
+              <InputField
+                label={i18n.t('books.bookName')}
+                error={errors.bookName}
+                placeholder={i18n.t('books.bookName')}
+                value={values.bookName}
+                onChangeText={handleChange('bookName')}
+                onBlur={handleBlur('bookName')}
+                keyboardType="default"
+              />
+              <Button label={i18n.t('books.create')} onPress={handleSubmit} disabled={!isValid} />
+            </>
+          )}
+        </Formik>
       </Container>
     </>
   )
