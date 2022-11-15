4c7c0346566972900805734c15f7271ad4114258
Ramu Ramasamy <rram.ramasamy@gmail.com>
2022-11-15 13:52:58 +0530

    create book updated

--- src/screens/Books/CreateBook.tsx
+++ src/screens/Books/CreateBook.tsx
@@ -2,40 +2,61 @@ import { Formik } from 'formik'
 import * as yup from 'yup'
 
 import { Container } from '../../components'
-import { Button, InputField } from '../../components/atoms'
-import { Header } from '../../components/fragments'
+import { Button } from '../../components/atoms'
+import { Checkbox, Header, InputField } from '../../components/fragments'
 import { i18n } from '../../locales'
+import { CurrencyPicker } from './components/CurrencyPicker'
 
 export const CreateBook = () => {
+  const initialValues = { bookName: '', currencyCode: '', currencySymbol: '', isDefault: false }
+
   const bookValidationSchema = yup.object().shape({
     bookName: yup.string().required(i18n.t('books.errorBookName')),
+    currencyCode: yup.string().required(),
+    currencySymbol: yup.string().required(),
+    isDefault: yup.boolean().default(false),
   })
 
   return (
-    <>
-      <Container>
-        <Header title={i18n.t('books.createBook')} backButton />
+    <Container>
+      <Header title={i18n.t('books.createBook')} backButton />
 
-        <Formik
-          validationSchema={bookValidationSchema}
-          initialValues={{ bookName: '' }}
-          onSubmit={(values) => console.log(values)}>
-          {({ handleChange, handleBlur, handleSubmit, values, errors, isValid }) => (
-            <>
-              <InputField
-                label={i18n.t('books.bookName')}
-                error={errors.bookName}
-                placeholder={i18n.t('books.bookName')}
-                value={values.bookName}
-                onChangeText={handleChange('bookName')}
-                onBlur={handleBlur('bookName')}
-                keyboardType="default"
-              />
-              <Button label={i18n.t('books.create')} onPress={handleSubmit} disabled={!isValid} />
-            </>
-          )}
-        </Formik>
-      </Container>
-    </>
+      <Formik
+        validationSchema={bookValidationSchema}
+        initialValues={initialValues}
+        onSubmit={(values) => console.log(values)}>
+        {({ handleChange, handleBlur, handleSubmit, setFieldValue, values, errors, isValid }) => (
+          <>
+            <InputField
+              label={i18n.t('books.bookName')}
+              error={errors.bookName}
+              placeholder={i18n.t('books.bookName')}
+              value={values.bookName}
+              onChangeText={handleChange('bookName')}
+              onBlur={handleBlur('bookName')}
+              keyboardType="default"
+              autoFocus
+            />
+
+            <CurrencyPicker
+              value={values.currencyCode}
+              onChange={(code, symbol) => {
+                setFieldValue('currencyCode', code)
+                setFieldValue('currencySymbol', symbol)
+              }}
+            />
+
+            <Checkbox
+              label="Default"
+              value={values.isDefault}
+              onValueChange={(value) => setFieldValue('isDefault', value)}
+              color={values.isDefault ? 'grey' : ''}
+            />
+
+            <Button label={i18n.t('books.create')} onPress={handleSubmit} disabled={!isValid} />
+          </>
+        )}
+      </Formik>
+    </Container>
   )
 }
