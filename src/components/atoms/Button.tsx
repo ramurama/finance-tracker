957d09de9b49e9df4d9768dcd69699002a8f28a3
Ramu Ramasamy <rram.ramasamy@gmail.com>
2022-11-15 14:13:54 +0530

    button component updated for optional style prop

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,18 +1,19 @@
 import { FC } from 'react'
-import { StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { FlexStyle, StyleSheet, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
 
 import { useTheme } from '../../theme'
 
 export type ButtonProps = {
   label: string
+  style?: FlexStyle
 } & TouchableOpacityProps
 
-export const Button: FC<ButtonProps> = ({ label, ...props }) => {
+export const Button: FC<ButtonProps> = ({ label, style, ...props }) => {
   const { colors } = useTheme()
 
   return (
     <TouchableOpacity
-      style={{ ...styles.button, backgroundColor: colors.secondaryText }}
+      style={[{ ...styles.button, backgroundColor: colors.secondaryText }, style]}
       {...props}>
       <Text style={{ ...styles.label, color: colors.background }}>{label}</Text>
     </TouchableOpacity>
