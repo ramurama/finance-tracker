5aa2b0a86a5e70ff1ef45ba0161262cc38d6a234
Ramu Ramasamy <rram.ramasamy@gmail.com>
2022-11-18 20:16:11 +0530

    list item margin

--- src/screens/Books/components/BookItem.tsx
+++ src/screens/Books/components/BookItem.tsx
@@ -79,6 +79,8 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
     borderBottomWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
   },
   iconView: {
     flex: 1,
--- src/screens/Categories/components/CategoryItem.tsx
+++ src/screens/Categories/components/CategoryItem.tsx
@@ -45,6 +45,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     height: 80,
     borderBottomWidth: 1,
+    marginLeft: 10,
+    marginRight: 10,
   },
   emojiContainer: {
     flexDirection: 'row',
