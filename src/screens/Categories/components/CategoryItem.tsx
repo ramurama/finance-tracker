d4a4d836fcd085a4d1067819d6349ef53e1f19c1
Ramu Ramasamy <rram.ramasamy@gmail.com>
2022-11-18 16:35:02 +0530

    category item component

---
+++ src/screens/Categories/components/CategoryItem.tsx
@@ -0,0 +1,73 @@
+import { FC } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+import { useTheme } from '../../../theme'
+import { TransactionType } from '../../../types'
+
+export type CategoryItemProps = {
+  name: string
+  type: TransactionType
+  emoji: string
+  onPress: () => void
+  onLongPress: () => void
+}
+
+export const CategoryItem: FC<CategoryItemProps> = ({ name, emoji, onPress, onLongPress }) => {
+  const { colors } = useTheme()
+
+  const Emoji = () => (
+    <View style={styles.emojiContainer}>
+      <View style={{ ...styles.emojiView, borderColor: colors.borderColor }}>
+        <Text style={styles.emoji}>{emoji}</Text>
+      </View>
+    </View>
+  )
+
+  const Content = () => (
+    <View style={styles.content}>
+      <Text style={{ ...styles.name, color: colors.foreground }}>{name}</Text>
+    </View>
+  )
+
+  return (
+    <TouchableOpacity
+      style={{ ...styles.container, borderBottomColor: colors.borderColor }}
+      onPress={onPress}
+      onLongPress={onLongPress}>
+      <Emoji />
+      <Content />
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    height: 80,
+    borderBottomWidth: 1,
+  },
+  emojiContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  emojiView: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 4,
+  },
+  emoji: {
+    fontSize: 30,
+  },
+  content: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flex: 4,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+})
