35dea7386b01c6f18f96eb071385f8729605af70
Ramu Ramasamy <rram.ramasamy@gmail.com>
2022-11-20 23:54:06 +0530

    reports screen made empty

--- src/screens/Reports/Reports.tsx
+++ src/screens/Reports/Reports.tsx
@@ -1,14 +1,9 @@
-import { Text } from 'react-native'
-
 import { Container } from '../../components'
-import { useTheme } from '../../theme'
 
 export const Reports = () => {
-  const { colors } = useTheme()
-
   return (
     <Container>
-      <Text style={{ color: colors.foreground }}>Reports screen</Text>
+      <></>
     </Container>
   )
 }
